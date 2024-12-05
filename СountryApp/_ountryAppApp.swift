44dc163a1e7498387fdb2307fcb05dc6b9f0df61
Danil Anikin <mart4r.clearlist@gmail.com>
2024-12-06 09:09:43 +1000

    Initial Commit

---
+++ СountryApp/_ountryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  _ountryAppApp.swift
+//  СountryApp
+//
+//  Created by Данил Аникин on 06.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct _ountryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
